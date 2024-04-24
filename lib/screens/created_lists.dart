bd660ebcfaf2330d3acf3898751387527805cd52
ATAKONE\atevilson.araujo <atevilson.araujo@atak.com.br>
2024-04-24 00:42:37 -0300

    add future build and method switch case

--- lib/screens/created_lists.dart
+++ lib/screens/created_lists.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/database/list_database.dart';
+import 'package:my_app/models/new_lists.dart';
 import 'package:my_app/screens/list_form.dart';
 
 class createdLists extends StatelessWidget {
-  const createdLists({super.key});
-
+  //final List<NewLists> list = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,17 +13,35 @@ class createdLists extends StatelessWidget {
         foregroundColor: Colors.white,
         title: Text('Listas de compras'),
       ),
-      body: ListView(
-        children: [
-          Card(
-            child: ListTile(
-              title: Text(
-                'Lista 1',
-                style: TextStyle(fontSize: 24.0),
-              ),
-            ),
-          )
-        ],
+      body: FutureBuilder<List<NewLists>>(
+        initialData: List.empty(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [CircularProgressIndicator(), Text('Carregando')],
+                ),
+              );
+            case ConnectionState.done:
+              final List<NewLists> list = snapshot.data ?? [];
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final NewLists lists = list[index];
+                  return _collectionsLists(lists);
+                },
+                itemCount: list.length,
+              );
+            case ConnectionState.active:
+              break;
+          }
+          return Text('Erro desconhecido');
+        },
+        future: findAll(),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -40,6 +59,24 @@ class createdLists extends StatelessWidget {
         foregroundColor: Colors.white,
         hoverColor: Colors.lightGreen,
         child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class _collectionsLists extends StatelessWidget {
+  final NewLists list;
+
+  _collectionsLists(this.list);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          list.nameList,
+          style: TextStyle(fontSize: 24.0),
+        ),
       ),
     );
   }
