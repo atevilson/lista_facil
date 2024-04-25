9c9ee172cb51b80b4ae6fac9d5bc070624a97060
ATAKONE\atevilson.araujo <atevilson.araujo@atak.com.br>
2024-04-25 11:55:04 -0300

    refactoring list_database

--- lib/database/list_database.dart
+++ lib/database/list_database.dart
@@ -1,11 +1,10 @@
+import 'package:my_app/database/dao/create_list_dao.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 Future<Database> getDataBase() async {
   final String path = join(await getDatabasesPath(), 'lista_facil2.db');
   return openDatabase(path, onCreate: (db, version) {
-    db.execute('CREATE TABLE new_lists('
-        'id INTEGER PRIMARY KEY, '
-        'name TEXT )');
+    db.execute(ListsDao.tableSQL);
   }, version: 1);
 }
