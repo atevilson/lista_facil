c92ad30435618491e8a692c93f8b7a68ea92afad
ATAKONE\atevilson.araujo <atevilson.araujo@atak.com.br>
2024-04-25 15:14:01 -0300

    organizing screens :-p

---
+++ lib/screens/app_home.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/screens/create_list/shopping_lists.dart';
+
+const _titleAppBar = 'Lista Fácil';
+
+class listCollections extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 21, 92, 24),
+          foregroundColor: Colors.white,
+          title: const Text(_titleAppBar),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(14.0),
+              child: Image.asset('images/carrinho_compras.jpg'),
+            ),
+            Material(
+              color: const Color.fromARGB(255, 90, 176, 23),
+              child: InkWell(
+                onTap: () {
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => createdLists()));
+                },
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Text(
+                        'Criar lista',
+                        style: TextStyle(color: Colors.white, fontSize: 19.0),
+                      ),
+                      Icon(
+                        Icons.shopping_cart,
+                        color: Colors.white,
+                        size: 29.0,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
