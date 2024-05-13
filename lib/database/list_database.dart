c9bb46d6aff3b26a2ae6fc4bbe2d94e14a71bc12
atevilson <freitas.atevilson@gmail.com>
2024-05-13 19:59:14 -0300

    add table for itens

--- lib/database/list_database.dart
+++ lib/database/list_database.dart
@@ -1,10 +1,12 @@
+import 'package:my_app/database/dao/create_itens_dao.dart';
 import 'package:my_app/database/dao/create_list_dao.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 Future<Database> getDataBase() async {
-  final String path = join(await getDatabasesPath(), 'lista_facil2.db');
+  final String path = join(await getDatabasesPath(), 'lista_facil4.db');
   return openDatabase(path, onCreate: (db, version) {
     db.execute(ListsDao.tableSQL);
+    db.execute(ItemsDao.tableSQLitens);
   }, version: 1);
 }
