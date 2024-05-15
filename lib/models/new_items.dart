c964efc042bcc14924dd4dd24734a7164e89412e
atevilson <freitas.atevilson@gmail.com>
2024-05-14 22:24:19 -0300

    update model for fk table

--- lib/models/new_items.dart
+++ lib/models/new_items.dart
@@ -2,11 +2,12 @@ class NewItems {
   final int? id;
   final String items;
   final int quantity;
+  int? listId;
 
-  NewItems({this.id, required this.items, required this.quantity});
+  NewItems({this.id, required this.items, required this.quantity, this.listId});
 
   @override
   String toString() {
-    return 'NewLists{items: $id, $items, $quantity}';
+    return 'NewLists{items: $id, $items, $quantity, $listId}';
   }
 }
