c40fe40d91479ac734b63a788310d652be68c6c7
atevilson <freitas.atevilson@gmail.com>
2024-05-16 13:03:32 -0300

    removal of private, and reference to the relationship between tables

--- lib/database/dao/create_itens_dao.dart
+++ lib/database/dao/create_itens_dao.dart
@@ -1,20 +1,21 @@
+import 'package:my_app/database/dao/create_list_dao.dart';
 import 'package:my_app/database/list_database.dart';
 import 'package:my_app/models/new_items.dart';
 import 'package:sqflite/sqflite.dart';
 
 class ItemsDao {
-  static const String _nameTable = 'new_itens';
-  static const String _id = 'id';
-  static const String _item = 'item';
-  static const String _quantity = 'quantity';
-  static const String _listId = 'list_id';
+  static const String nameTable = 'new_itens';
+  static const String id = 'id';
+  static const String item = 'item';
+  static const String quantity = 'quantity';
+  static const String listId = 'list_id';
 
-  static const String tableSQLitens = 'CREATE TABLE $_nameTable('
-      '$_id INTEGER PRIMARY KEY AUTOINCREMENT, '
-      '$_item TEXT,'
-      '$_quantity INTEGER,'
-      '$_listId INTEGER NOT NULL,'
-      'FOREIGN KEY ($_listId) REFERENCES new_lists ($_id))';
+  static const String tableSQLitens = 'CREATE TABLE $nameTable('
+      '$id INTEGER PRIMARY KEY AUTOINCREMENT, '
+      '$item TEXT,'
+      '$quantity INTEGER,'
+      '$listId INTEGER,'
+      'FOREIGN KEY ($listId) REFERENCES ${ListsDao.nameTable} (${ListsDao.id}))';
 
   Future<int> save(NewItems items) async {
     final Database db = await getDataBase();
@@ -27,24 +28,39 @@ class ItemsDao {
 
   Future<int> _toList(NewItems items, Database db) {
     final Map<String, dynamic> newItem = {};
-    newItem[_id] = items.id;
-    newItem[_item] = items.items;
-    newItem[_quantity] = items.quantity;
-    newItem[_listId] = items.listId;
-    return db.insert(_nameTable, newItem);
+    newItem[id] = items.id;
+    newItem[item] = items.items;
+    newItem[quantity] = items.quantity;
+    newItem[listId] = items.listId;
+    return db.insert(nameTable, newItem);
   }
 
   Future<List<NewItems>> findAll() async {
     final Database db = await getDataBase();
-    final List<Map<String, dynamic>> result = await db.query(_nameTable);
+    final List<Map<String, dynamic>> result = await db.query(nameTable);
     final List<NewItems> items = [];
     for (Map<String, dynamic> row in result) {
       final NewItems newItem = NewItems(
-        id: row[_id],
-        items: row[_item],
-        quantity: row[_quantity],
-          listId: row[_listId]
-      );
+          id: row[id],
+          items: row[item],
+          quantity: row[quantity],
+          listId: row[listId]);
+      items.add(newItem);
+    }
+    return items;
+  }
+
+  Future<List<NewItems>> findByListId(int value) async {
+    final Database db = await getDataBase();
+    final List<Map<String, dynamic>> result =
+        await db.query(nameTable, where: " $listId = $value");
+    final List<NewItems> items = [];
+    for (Map<String, dynamic> row in result) {
+      final NewItems newItem = NewItems(
+          id: row[id],
+          items: row[item],
+          quantity: row[quantity],
+          listId: row[listId]);
       items.add(newItem);
     }
     return items;
