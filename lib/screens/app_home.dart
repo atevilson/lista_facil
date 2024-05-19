4b075d2e272910423af8826041133a892f8df413
atevilson <freitas.atevilson@gmail.com>
2024-05-19 20:41:47 -0300

    add boxShadow in container buttom

--- lib/screens/app_home.dart
+++ lib/screens/app_home.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:my_app/screens/create_list/shopping_lists.dart';
 import 'package:my_app/utils_colors/utils_style.dart';
 
@@ -11,6 +12,11 @@ class listCollections extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+
     return MaterialApp(
       home: Scaffold(
         appBar: appBarCustom(title: _titleAppBar),
@@ -29,7 +35,7 @@ class listCollections extends StatelessWidget {
                 scrollDirection: Axis.horizontal,
                 children: <Widget> [
                   _MenuItemHome("Criar Lista", 
-                  Icons.shopping_cart, 
+                  Icons.list_alt_outlined, 
                     onClick: () => _pageCreatedLists(context),
                   ),
                   _MenuItemHome(
@@ -76,6 +82,16 @@ class _MenuItemHome extends StatelessWidget {
         child: InkWell(
           onTap: () => onClick(),
           child: Container(
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: UtilColors.instance.colorWhite.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 4,
+                  offset: Offset(0,2)
+                )
+              ]
+            ),
             padding: const EdgeInsets.all(8.0),
             width: 150,
             child: Column(
