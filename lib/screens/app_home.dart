2c96ad1d655b9a2b577835d70227469c8a0392e4
atevilson <freitas.atevilson@gmail.com>
2024-05-19 20:18:36 -0300

    add class fot items menu home

--- lib/screens/app_home.dart
+++ lib/screens/app_home.dart
@@ -1,7 +1,8 @@
 
+
 import 'package:flutter/material.dart';
-import 'package:my_app/database/list_database.dart';
 import 'package:my_app/screens/create_list/shopping_lists.dart';
+import 'package:my_app/utils_colors/utils_style.dart';
 
 const _titleAppBar = 'Lista Fácil';
 
@@ -12,15 +13,7 @@ class listCollections extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.red,
-          foregroundColor: Colors.white,
-          title: const Text(_titleAppBar, style: TextStyle(
-            fontSize: 24.0,
-            fontWeight: FontWeight.w500
-          ),),
-          titleSpacing: 8.0,
-        ),
+        appBar: appBarCustom(title: _titleAppBar),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -29,40 +22,82 @@ class listCollections extends StatelessWidget {
               padding: const EdgeInsets.only(top: 140.0),
               child: Image.asset('images/carrinho_compras.jpg'),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Material(
-                color: Colors.red,
-                child: InkWell(
-                  onTap: () {
-                    Navigator.of(context).push(
-                        MaterialPageRoute(builder: (context) => createdLists()));
-                        listarListas();
-                        listarItens();
-                        
-                  },
-                  child:  SizedBox(
-                    height: 100,
-                    width: 150,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text(
-                          'Criar lista',
-                          style: TextStyle(color: Colors.white, fontSize: 19.0),
-                        ),
-                        Icon(
-                          Icons.shopping_cart,
-                          color: Colors.white,
-                          size: 29.0,
-                        )
-                      ],
-                    ),
+            Container(
+              color: UtilColors.instance.colorContainer,
+              height: 115,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: <Widget> [
+                  _MenuItemHome("Criar Lista", 
+                  Icons.shopping_cart, 
+                    onClick: () => _pageCreatedLists(context),
                   ),
+                  _MenuItemHome(
+                  'Confira ofertas',
+                  Icons.add_shopping_cart,
+                  onClick: () => (),
                 ),
+                ],
               ),
             )
           ],
+        ),
+      ),
+    );
+  }
+
+void _pageCreatedLists(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => createdLists(),
+      ),
+    );
+  }
+}
+
+
+// Itens cards
+class _MenuItemHome extends StatelessWidget {
+  final String name;
+  final IconData icon;
+  final Function onClick;
+
+  const _MenuItemHome(
+    this.name,
+    this.icon, {
+    required this.onClick,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        child: InkWell(
+          onTap: () => onClick(),
+          child: Container(
+            padding: const EdgeInsets.all(8.0),
+            width: 150,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  name,
+                  style: TextStyle(
+                    color: UtilColors.instance.colorRed,
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Icon(
+                  icon,
+                  color: UtilColors.instance.colorRed,
+                  size: 28.0,
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
