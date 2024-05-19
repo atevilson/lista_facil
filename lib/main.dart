7de5a1b6e91e626c2864af8b220c60037ac19faf
atevilson <freitas.atevilson@gmail.com>
2024-05-19 02:00:46 -0300

    set ThemeData

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/screens/app_home.dart';
+import 'package:my_app/utils_colors/utils_style.dart';
 
 void main() {
-  runApp(myApplication());
+  runApp(const myApplication());
 }
 
 class myApplication extends StatelessWidget {
+  const myApplication({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: listCollections(),
+      theme: ThemeData(
+        inputDecorationTheme:  InputDecorationTheme(
+          labelStyle: TextStyle(color: UtilColors.instance.colorRed),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: UtilColors.instance.colorRed, width: 2.0),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: UtilColors.instance.colorGray, width: 1.0),
+          ),
+          border: const OutlineInputBorder(),
+        ),
+      ),
+     home: const listCollections(),
     );
   }
 }
