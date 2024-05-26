966d7049867eabd6d7b83a7f99cd612f239cbf4f
atevilson <freitas.atevilson@gmail.com>
2024-05-26 20:28:59 -0300

    class name correction

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class myApplication extends StatelessWidget {
           border: const OutlineInputBorder(),
         ),
       ),
-     home: const listCollections(),
+     home: const ListCollections(),
     );
   }
 }
