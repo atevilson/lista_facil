a9651dc5f652363e30c8b3e588adf3bd7c3fb4a0
atevilson <atevilson.araujo@atak.com.br>
2024-10-28 14:37:46 -0300

    update custom colors

--- lib/components/data_entry_text.dart
+++ lib/components/data_entry_text.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lista_facil/components/custom_icons.dart';
+import 'package:lista_facil/utils_colors/utils_style.dart';
 
 class DataEntryText extends StatelessWidget {
   final TextEditingController itemController;
@@ -15,9 +16,23 @@ class DataEntryText extends StatelessWidget {
       padding: const EdgeInsets.all(9.0),
       child: TextField(
         controller: itemController,
+        cursorColor: UtilColors.colorBlack,
         decoration: InputDecoration(
-            icon: const Icon(CustomIcons.novoItem), labelText: itemLabel),
+            focusedBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: UtilColors.colorBlack),
+            ),
+            enabledBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: UtilColors.colorBlack),
+            ),
+            border: const OutlineInputBorder(
+              borderSide: BorderSide(color: UtilColors.colorBlack),
+            ),
+            labelStyle: const TextStyle(color: UtilColors.colorBlack),
+            hintStyle: const TextStyle(color: UtilColors.colorBlack),
+            icon:  const Icon(CustomIcons.novoItem),
+            labelText: itemLabel),
         keyboardType: TextInputType.text,
+        style: const TextStyle(fontSize: 22.0, color: UtilColors.colorBlack),
       ),
     );
   }
