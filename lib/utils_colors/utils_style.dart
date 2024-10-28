fbfda23419f44ae3ea209915c3d269fb488a1469
atevilson <atevilson.araujo@atak.com.br>
2024-10-27 22:04:45 -0300

    Update colors

--- lib/utils_colors/utils_style.dart
+++ lib/utils_colors/utils_style.dart
@@ -9,12 +9,12 @@ class UtilColors {
   static const Color colorWhite70 = Colors.white70;
   static const Color colorBlack = Colors.black;
   static const Color colorBlack45 = Colors.black45;
-  static const Color colorError = Color.fromARGB(255, 255, 0, 25);
+  static Color? colorRed800 = Colors.red[800];
   static const Color colorRed =  Colors.red;
   static const Color colorContainer = Color.fromARGB(255, 224, 5, 5);
   static const Color textFocus = Colors.transparent;
   static const Color colorGray = Colors.grey;
   static Color? colorGray600 = Colors.grey[600];
   static Color? colorBlueGrey800 = Colors.blueGrey[800];
-
+  static Color? colorGreenAccent400 = Colors.greenAccent[400];
   }
