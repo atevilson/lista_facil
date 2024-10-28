19955273eebdf61950767288b1a3bf28e407498c
atevilson <atevilson.araujo@atak.com.br>
2024-10-27 21:42:48 -0300

    mapped price and isChekd field

--- lib/models/new_items.dart
+++ lib/models/new_items.dart
@@ -2,9 +2,11 @@ class NewItems {
   final int? id;
   final String items;
   final int quantity;
+  double? price;
   int? listId;
+  bool? isChecked;
 
-  NewItems({this.id, required this.items, required this.quantity, this.listId});
+  NewItems({this.id, required this.items, required this.quantity, this.listId, this.price, this.isChecked});
 
   @override
   String toString() {
