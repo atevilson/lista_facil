bf014789b286f8fb007324a64e37523227af2a9c
atevilson <atevilson.araujo@atak.com.br>
2024-10-27 21:43:10 -0300

    mapped budget field

--- lib/models/new_lists.dart
+++ lib/models/new_lists.dart
@@ -1,8 +1,9 @@
 class NewLists {
   final int id;
   final String nameList;
+  final double? budget;
 
-  NewLists(this.id, this.nameList);
+  NewLists(this.id, this.nameList, this.budget);
 
   @override
   String toString() {
