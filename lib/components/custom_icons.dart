c4ef008816385453593cf68a7af2c04429c91e9b
atevilson <atevilson.araujo@atak.com.br>
2024-11-11 17:52:50 -0300

    update custom icons

--- lib/components/custom_icons.dart
+++ lib/components/custom_icons.dart
@@ -3,9 +3,9 @@ import 'package:flutter/widgets.dart';
 class CustomIcons {
 
   static const _keyFontFamily = 'CustomIcons';
-  static const IconData criarLista = IconData(59393, fontFamily: _keyFontFamily);
-  static const IconData listasCriadas = IconData(59392, fontFamily: _keyFontFamily);
-  static const IconData novoItem = IconData(59394, fontFamily: _keyFontFamily);
-  static const IconData quantidade = IconData(59395, fontFamily: _keyFontFamily);
+  //static const IconData criarLista = IconData(59393, fontFamily: _keyFontFamily);
+  static const IconData line = IconData(0xe900, fontFamily: _keyFontFamily);
+  //static const IconData novoItem = IconData(59394, fontFamily: _keyFontFamily);
+  //static const IconData quantidade = IconData(59395, fontFamily: _keyFontFamily);
 
 }
