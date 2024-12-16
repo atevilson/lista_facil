5667c55be523eb8b23839581f2938d9c111c7ed3
atevilson <atevilson.araujo@atak.com.br>
2024-12-15 21:55:17 -0300

    add mapeamento de icon de ordenação

--- lib/components/custom_icons.dart
+++ lib/components/custom_icons.dart
@@ -4,7 +4,7 @@ class CustomIcons {
 
   static const _keyFontFamily = 'CustomIcons';
   //static const IconData criarLista = IconData(59393, fontFamily: _keyFontFamily);
-  static const IconData line = IconData(0xe900, fontFamily: _keyFontFamily);
+  static const IconData alphabetic = IconData(0xE900, fontFamily: _keyFontFamily);
   //static const IconData novoItem = IconData(59394, fontFamily: _keyFontFamily);
   //static const IconData quantidade = IconData(59395, fontFamily: _keyFontFamily);
 
