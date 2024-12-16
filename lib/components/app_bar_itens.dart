16786c07115486f7562ed8253922813dff81ed85
atevilson <atevilson.araujo@atak.com.br>
2024-12-15 21:45:20 -0300

    add componente da tela de itens

---
+++ lib/components/app_bar_itens.dart
@@ -0,0 +1,263 @@
+
+import 'package:flutter/material.dart';
+import 'package:lista_facil/components/custom_icons.dart';
+import 'package:lista_facil/controllers/item_controller.dart';
+import 'package:lista_facil/utils_colors/utils_style.dart';
+
+class AppBarItens extends StatelessWidget {
+  final bool isSearchActive;
+  final VoidCallback onBack;
+  final VoidCallback onSearch;
+  final String title;
+  final TextEditingController? searchController;
+  final ValueChanged<String>? onSearchChanged;
+  final String hintText;
+  final Widget? child;
+  final Color? color;
+  final double? budget;
+  final ValueNotifier<double>? totalNotifier;
+  final ItemController controller; 
+
+  const AppBarItens({
+    super.key,
+    required this.isSearchActive,
+    required this.onBack,
+    required this.onSearch,
+    required this.title,
+    this.searchController,
+    this.onSearchChanged,
+    required this.hintText,
+    this.child,
+    this.color,
+    this.budget,
+    this.totalNotifier,
+    required this.controller 
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: color ?? ThemeColor.colorWhite,
+        boxShadow: [
+          BoxShadow(
+            color: ThemeColor.colorBlueGradient.withOpacity(0.9),
+            spreadRadius: 3,
+            blurRadius: 15,
+            offset: const Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 18),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                IconButton(
+                  onPressed: onBack,
+                  icon: const Icon(Icons.arrow_back),
+                  color: ThemeColor.colorBlueTema,
+                ),
+                Expanded(
+                  child: AnimatedSwitcher(
+                    duration: const Duration(milliseconds: 350),
+                    child: isSearchActive
+                        ? TextField(
+                            key: const ValueKey(1),
+                            controller: searchController,
+                            decoration: InputDecoration(
+                              hintText: hintText,
+                              hintStyle:
+                                 TextStyle(color: ThemeColor.colorBlueItemNaoSel),
+                              filled: true,
+                              fillColor: ThemeColor.colorWhite70,
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(25.0),
+                                borderSide: BorderSide(
+                                    color: ThemeColor.colorBlueTema, width: 1.5),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(25.0),
+                                borderSide: BorderSide(
+                                    color: ThemeColor.colorBlueTema, width: 1.5),
+                              ),
+                            ),
+                            style: TextStyle(color: ThemeColor.colorBlueTema),
+                            onChanged: onSearchChanged,
+                          )
+                        : Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 20.0),
+                          child: Text(
+                              title,
+                              key: const ValueKey(2),
+                              style: TextStyle(
+                                color: ThemeColor.colorBlueTema,
+                                fontSize: 24,
+                                fontWeight: FontWeight.w400
+                              ),
+                              textAlign: TextAlign.start,
+                            ),
+                        ),
+                  ),
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: onSearch,
+                      icon: Icon(
+                        isSearchActive ? Icons.close : Icons.search,
+                        color: isSearchActive
+                            ? ThemeColor.colorBlueItemNaoSel
+                            : ThemeColor.colorBlueTema,
+                      ),
+                    ),
+                    PopupMenuButton<int>(
+                      position: PopupMenuPosition.over,
+                      color: ThemeColor.colorBlueTema,
+                      icon: Icon(
+                        Icons.more_vert,
+                        color: ThemeColor.colorBlueTema,
+                      ),
+                      onSelected: (value) {
+                        switch (value) {
+                          case 1:
+                            controller.sortItems(!controller.isAscending);
+                            break;
+                          case 2:
+                            controller.sortItems(controller.isAscending);
+                            break;
+                          default:
+                            break;
+                        }
+                      },
+                      itemBuilder: (context) => [
+                        PopupMenuItem(
+                            value: 1,
+                            child: Row(
+                              mainAxisSize: MainAxisSize.max,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text("Ordenar",
+                                style: TextStyle(
+                                  color: ThemeColor.colorWhite
+                                ),),
+                                IconButton(
+                                    iconSize: 28,
+                                    onPressed: () => controller
+                                        .sortItems(!controller.isAscending),
+                                    icon: Icon(CustomIcons.alphabetic),
+                                    color: ThemeColor.colorWhite),
+                              ],
+                            )),
+                        PopupMenuItem(
+                            value: 2,
+                            child: Row(
+                              children: [
+                                GestureDetector(
+                                  behavior: HitTestBehavior.opaque,
+                                  onTap: () => controller.shareItems(
+                                      controller.quantityItems.value),
+                                  child: Row(
+                                    children: [
+                                      Text(
+                                        "Compartilhar",
+                                        style: TextStyle(
+                                            color: ThemeColor.colorWhite),
+                                      ),
+                                      SizedBox(width: 20,),
+                                      Icon(
+                                        Icons.share,
+                                        size: 28,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ))
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(height: 10),
+            // Seu conteúdo adicional
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                  decoration: const BoxDecoration(
+                    color: ThemeColor.colorWhite,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        budget != null ? "Orçamento " : "Orçamento não definido",
+                        style: const TextStyle(
+                          fontSize: 17.0,
+                          color: ThemeColor.colorBlue,
+                        ),
+                      ),
+                      Text(
+                        budget != null ? "R\$ $budget" : "",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.w500,
+                          color: ThemeColor.colorBlueTema,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                if (totalNotifier != null)
+                  ValueListenableBuilder<double>(
+                    valueListenable: totalNotifier!,
+                    builder: (context, total, _) {
+                      Color? colorTotalGasto;
+
+                      if (budget != null && total > budget!) {
+                        colorTotalGasto = ThemeColor.colorRed800;
+                      } else {
+                        colorTotalGasto = ThemeColor.colorGreenTotal;
+                      }
+
+                      return Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                        decoration: const BoxDecoration(
+                          color: ThemeColor.colorWhite,
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Expanded(
+                              child: const Text(
+                                "Total gasto",
+                                style: TextStyle(
+                                  fontSize: 17.0,
+                                  color: ThemeColor.colorBlue,
+                                ),
+                              ),
+                            ),
+                            Text(
+                              "R\$ $total",
+                              style: TextStyle(
+                                fontSize: 20.0,
+                                color: colorTotalGasto,
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
