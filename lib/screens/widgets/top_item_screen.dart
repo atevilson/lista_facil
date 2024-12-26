a30de021e26d8e87bdd8d25c8e72c87bed400b12
atevilson <atevilson.araujo@atak.com.br>
2024-12-25 21:42:04 -0300

    add tela de relatório de top 10

---
+++ lib/screens/widgets/top_item_screen.dart
@@ -0,0 +1,43 @@
+
+import 'package:flutter/material.dart';
+import 'package:lista_facil/utils_colors/utils_style.dart';
+
+class TopItemsScreen extends StatelessWidget {
+  final String title;
+  final List<MapEntry<String, int>> data;
+
+  const TopItemsScreen({
+    super.key,
+    required this.title,
+    required this.data,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title, style: TextStyle(
+          color: ThemeColor.colorWhite
+        ),), 
+      ),
+      body: ListView.builder(
+        itemCount: data.length,
+        itemBuilder: (context, index) {
+          final entry = data[index];
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListTile(
+              tileColor: ThemeColor.colorWhite70,
+              title: Text(entry.key, style: TextStyle(
+                color: ThemeColor.colorBlueTema
+              ),),
+              subtitle: Text("Quantidade: ${entry.value}", style: TextStyle(
+                color: ThemeColor.colorBlueTema
+              ),),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
