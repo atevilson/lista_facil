129580ef0e04a544ba71071c79d9bbbf389ee7c2
atevilson <freitas.atevilson@gmail.com>
2025-02-09 17:42:40 -0300

    ajusta inicalização da var _database

--- lib/data/services/local_db/sqlite/sqlite_service.dart
+++ lib/data/services/local_db/sqlite/sqlite_service.dart
@@ -9,37 +9,44 @@ class SqliteService implements IDatabaseService {
   Database? _database;
 
   @override
-  Future<void> init() async{
-    final String path = join(await getDatabasesPath(), "lista_facil.db");
-    _database = await openDatabase(path, version: 1, onCreate: (db, version) {
+  Future<Database> init() async{
+    if(_database != null) return _database!;
+    final String path = join(await getDatabasesPath(), 'lista_facil1.db');
+     _database = await openDatabase(path, version: 1, onCreate: (db, version) {
       db.execute(ListsDao.tableSQL);
       db.execute(ItemsDao.tableSQLitens);
     });
+    return _database!;
   }
 
   @override
   Future<int> insert(String table, Map<String, dynamic> data) async{
-    return await _database!.insert(table, data);
+    final Database db = await init();
+    return db.insert(table, data); 
   }
 
   @override
   Future<int> update(String table, Map<String, dynamic> data, String column, value) async{
-    return await _database!.update(table, data, where: "$column = ?", whereArgs: [value]);
+    final Database db = await init();
+    return db.update(table, data, where: "$column = ?", whereArgs: [value]);
   }
 
   @override
   Future<int> delet(String table, String coloumn, value) async{
-    return await _database!.delete(table, where: "$coloumn = ?", whereArgs: [value]);
+    final Database db = await init();
+    return db.delete(table, where: "$coloumn = ?", whereArgs: [value]);
   }
 
   @override
   Future<List<Map<String, dynamic>>> queryAll(String table) async{
-    return await _database!.query(table);
+    final Database db = await init();
+    return db.query(table);
   }
 
   @override
   Future<List<Map<String, dynamic>>> queryBy(String table, String column, value) async{
-    return  await _database!.query(table, where: "$column = ?", whereArgs: [value]);
+    final Database db = await init();
+    return db.query(table, where: "$column = ?", whereArgs: [value]);
   }
 
 }
